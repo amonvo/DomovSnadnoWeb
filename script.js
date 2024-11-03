2fff7298162c7e3f8e42e01d5fdda691da189a7f
amonv <vojtechamon@gmail.com>
2024-11-03 23:54:26 +0100

    Popis změn

--- script.js
+++ script.js
@@ -23,18 +23,30 @@ function scrollToSection() {
 // Inicializace IntersectionObserver pro sledování druhé sekce
 const observer = new IntersectionObserver((entries) => {
     const button = document.querySelector(".scroll-button");
-    const text = document.querySelector(".tree-text");
 
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             button.style.display = "none"; // Skryje tlačítko, když je druhá sekce viditelná
-            text.classList.add("hidden"); // Skryje text
         } else {
             button.style.display = "block"; // Zobrazí tlačítko, pokud je druhá sekce mimo zorné pole
-            text.classList.remove("hidden"); // Zobrazí text, pokud jsme v první sekci
         }
     });
 }, { threshold: 0.1 }); // Prahová hodnota (10 % druhé sekce v zorném poli)
 
 // Sleduje druhou sekci pro změny viditelnosti
 observer.observe(document.querySelector('.section:nth-child(2)'));
+
+
+function scrollToSection() {
+    document.querySelector('.section:nth-child(2)').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+// Zajistí, že se přejde na třetí sekci po kliknutí na Firmy
+document.querySelector('.navbar a[href="#third-section"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#third-section').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
